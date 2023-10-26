6735a4dc7f609cfa4fdcd73eef470878c35bc3ae
miz8462 <wwr8462@gmail.com>
2023-10-26 16:00:30 +0900

    SingleChildScrollViewを導入し、画面からはみ出すエラーを解消

--- lib/main.dart
+++ lib/main.dart
@@ -66,142 +66,144 @@ class _InsomniaRecordHomePageState extends State<InsomniaRecordHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Column(
-            children: [
-              const Text('布団に入った時間'),
-              TextField(
-                onChanged: (text) {
-                  timeForBed = text;
-                },
-                decoration: InputDecoration(
-                  hintText: timeForBed,
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text('布団から出た時間'),
-              TextField(
-                onChanged: (text) {
-                  wakeUpTime = text;
-                },
-                decoration: InputDecoration(
-                  hintText: wakeUpTime,
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text('寝付くまでの時間'),
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (text) {
-                  sleepTime = int.parse(text);
-                },
-                decoration: InputDecoration(
-                  hintText: sleepTime.toString(),
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text('夜中目覚めた回数'),
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (text) {
-                  numberOfAwaking = int.parse(text);
-                },
-                decoration: InputDecoration(
-                  hintText: numberOfAwaking.toString(),
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text('夜中目覚めてた時間'),
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (text) {
-                  timeOfAwaking = int.parse(text);
-                },
-                decoration: InputDecoration(
-                  hintText: timeOfAwaking.toString(),
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text('朝の気分(5段階)'),
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (text) {
-                  morningFeeling = int.parse(text);
-                },
-                decoration: InputDecoration(
-                  hintText: morningFeeling.toString(),
-                ),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const Text('睡眠の質(5段階)'),
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                onChanged: (text) {
-                  qualityOfSleep = int.parse(text);
-                },
-                decoration: InputDecoration(
-                  hintText: qualityOfSleep.toString(),
-                ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // データを登録
-
-              sleepRecordBox?.put(
-                SleepRecord(
-                  timeForBed: timeForBed,
-                  wakeUpTime: wakeUpTime,
-                  sleepTime: sleepTime,
-                  numberOfAwaking: numberOfAwaking,
-                  timeOfAwaking: timeOfAwaking,
-                  morningFeeling: morningFeeling,
-                  qualityOfSleep: qualityOfSleep,
-                ),
-              );
-              sleepRecords = sleepRecordBox?.getAll() ?? [];
-              setState(() {});
-              // 画面遷移
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return RecordTablePage(sleepRecords: sleepRecords);
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Column(
+              children: [
+                const Text('布団に入った時間'),
+                TextField(
+                  onChanged: (text) {
+                    timeForBed = text;
                   },
+                  decoration: InputDecoration(
+                    hintText: timeForBed,
+                  ),
                 ),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.orange.shade200,
-              foregroundColor: Colors.brown.shade800,
+              ],
             ),
-            child: const Text("登録"),
-          ),
-        ],
+            Column(
+              children: [
+                const Text('布団から出た時間'),
+                TextField(
+                  onChanged: (text) {
+                    wakeUpTime = text;
+                  },
+                  decoration: InputDecoration(
+                    hintText: wakeUpTime,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text('寝付くまでの時間'),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    sleepTime = int.parse(text);
+                  },
+                  decoration: InputDecoration(
+                    hintText: sleepTime.toString(),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text('夜中目覚めた回数'),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    numberOfAwaking = int.parse(text);
+                  },
+                  decoration: InputDecoration(
+                    hintText: numberOfAwaking.toString(),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text('夜中目覚めてた時間'),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    timeOfAwaking = int.parse(text);
+                  },
+                  decoration: InputDecoration(
+                    hintText: timeOfAwaking.toString(),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text('朝の気分(5段階)'),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    morningFeeling = int.parse(text);
+                  },
+                  decoration: InputDecoration(
+                    hintText: morningFeeling.toString(),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                const Text('睡眠の質(5段階)'),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  onChanged: (text) {
+                    qualityOfSleep = int.parse(text);
+                  },
+                  decoration: InputDecoration(
+                    hintText: qualityOfSleep.toString(),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // データを登録
+
+                sleepRecordBox?.put(
+                  SleepRecord(
+                    timeForBed: timeForBed,
+                    wakeUpTime: wakeUpTime,
+                    sleepTime: sleepTime,
+                    numberOfAwaking: numberOfAwaking,
+                    timeOfAwaking: timeOfAwaking,
+                    morningFeeling: morningFeeling,
+                    qualityOfSleep: qualityOfSleep,
+                  ),
+                );
+                sleepRecords = sleepRecordBox?.getAll() ?? [];
+                setState(() {});
+                // 画面遷移
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecordTablePage(sleepRecords: sleepRecords);
+                    },
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange.shade200,
+                foregroundColor: Colors.brown.shade800,
+              ),
+              child: const Text("登録"),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/record_table_page.dart
+++ lib/record_table_page.dart
@@ -22,19 +22,21 @@ class _RecordTablePageState extends State<RecordTablePage> {
         itemBuilder: (context, index) {
           Text(widget.sleepRecords[index].timeForBed);
           final sleepRecord = widget.sleepRecords[index];
-          return Column(
-            children: [
-              Text('布団に入った時間: ${sleepRecord.timeForBed}'),
-              Text('布団から出た時間: ${sleepRecord.wakeUpTime}'),
-              Text('眠りにつくまでの時間: ${sleepRecord.sleepTime.toString()}'),
-              Text('夜中に目覚めた回数: ${sleepRecord.numberOfAwaking.toString()}'),
-              Text('夜中に目覚めてた時間: ${sleepRecord.timeOfAwaking.toString()}'),
-              Text('朝の体調(5段階): ${sleepRecord.morningFeeling.toString()}'),
-              Text('睡眠の質(5段階): ${sleepRecord.qualityOfSleep.toString()}'),
-              const SizedBox(
-                height: 10,
-              )
-            ],
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('布団に入った時間: ${sleepRecord.timeForBed}'),
+                Text('布団から出た時間: ${sleepRecord.wakeUpTime}'),
+                Text('眠りにつくまでの時間: ${sleepRecord.sleepTime.toString()}'),
+                Text('夜中に目覚めた回数: ${sleepRecord.numberOfAwaking.toString()}'),
+                Text('夜中に目覚めてた時間: ${sleepRecord.timeOfAwaking.toString()}'),
+                Text('朝の体調(5段階): ${sleepRecord.morningFeeling.toString()}'),
+                Text('睡眠の質(5段階): ${sleepRecord.qualityOfSleep.toString()}'),
+                const SizedBox(
+                  height: 10,
+                )
+              ],
+            ),
           );
         },
       ),
